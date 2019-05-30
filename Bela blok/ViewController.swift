8c86efa354461252da33d70073e5bceb14e598c9
Josip Zidar <josip.zidar@gauss.hr>
2019-05-30 08:45:13 +0200

    Initial commit

---
+++ Bela blok/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Bela blok
+//
+//  Created by Josip Zidar on 29/05/2019.
+//  Copyright © 2019 Josip Zidar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var newGameButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       configUI()
+    }
+    
+    private func configUI() {
+        rrr
+    }
+    
+    @IBAction func onTouchNewGameButton(_ sender: Any) {
+        
+    }
+    
+}
+
